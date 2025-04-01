2091ac5a525ce6cc8bf13b68bdfc1c9a23b68c62
Zacattack <whimper_robin0z@icloud.com>
2025-04-01 13:51:03 -0400

    Initial Commit

---
+++ MCP Assistant/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MCP Assistant
+//
+//  Created by Zac Hixon on 4/1/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
